5cf0736637caab5ffe5f311b80df1db17bf47b0b
Natalia Garrido <natgramirez@outlook.com>
2018-05-13 16:24:04 -0400

    adding addtask  method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
+  tasks = [];
+  // desde ngModel
+  taskValue = '';
+
+  addTask() {
+    // valor del input -> this.taskValue
+    this.tasks.push(this.taskValue);
+    this.taskValue = '';
+  }
 }
